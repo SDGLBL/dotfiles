5baef3710dd26036a8b265d2b26065265185668d
SDGLBL <lijieisgugu@gmail.com>
2022-03-19 10:17:51 +0800

    :wrench: Update lunarvim configuration
    as title

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -4,10 +4,10 @@ lvim.format_on_save = true
 lvim.colorscheme = "onedarker"
 lvim.lint_on_save = true
 lvim.transparent_window = true
---lvim.colorscheme = 'sonokai'
+lvim.colorscheme = 'sonokai'
 -- lvim.colorscheme = 'monokai_soda'
 --lvim.colorscheme = 'onedarkpro'
-lvim.colorscheme = 'tokyonight'
+-- lvim.colorscheme = 'tokyonight'
 -- lvim.colorscheme = 'rose-pine'
 
 -- colorscheme sonokai style
@@ -45,17 +45,17 @@ vim.g.copilot_tab_fallback = ""
 
 -- Please check https://github.com/equalsraf/win32yank
 -- If you use wsl2 or wsl in windows
-vim.g["clipboard"] = {
-  ["name"] = "win32yank-wsl",
-  ['copy']= {
-      ['+']= '/mnt/c/Users/lijie/win32yank.exe -i --crlf',
-      ['*']= '/mnt/c/Users/lijie/win32yank.exe -i --crlf',
-  },
-  ['paste']= {
-      ['+']= '/mnt/c/Users/lijie/win32yank.exe -o --lf',
-      ['*']= '/mnt/c/Users/lijie/win32yank.exe -o --lf',
-  },
-}
+-- vim.g["clipboard"] = {
+--   ["name"] = "win32yank-wsl",
+--   ['copy']= {
+--       ['+']= '/mnt/c/Users/lijie/win32yank.exe -i --crlf',
+--       ['*']= '/mnt/c/Users/lijie/win32yank.exe -i --crlf',
+--   },
+--   ['paste']= {
+--       ['+']= '/mnt/c/Users/lijie/win32yank.exe -o --lf',
+--       ['*']= '/mnt/c/Users/lijie/win32yank.exe -o --lf',
+--   },
+-- }
 
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
@@ -65,7 +65,7 @@ lvim.keys.normal_mode = {
   -- Disable Ex mode, because what the fuck is that...
   ["Q"] = "<NOP>",
   -- Alternative way to save
-  ["<C-s>"] = ":w<CR>",
+  ["<C-s>"] = ":SymbolsOutline<CR>",
   -- Use CTRL+C instead of <ESC>...
   ["<C-c>"] = "<ESC>",
 
@@ -196,8 +196,108 @@ require("nvim-treesitter.configs").setup {
     }
 }
 
--- generic LSP settings
+local dap = require("dap")
 
+-- go debug
+dap.adapters.go = {
+  type = 'executable';
+  command = 'node';
+  args ={ '/home/lijie/.local/share/nvim/dapinstall/go/vscode-go/dist/debugAdapter.js'};
+}
+dap.configurations.go = {
+    {
+        type = 'go';
+        name = 'Debug';
+        request = 'launch';
+        showLog = false;
+        program = "${file}";
+        dlvToolPath = vim.fn.exepath('dlv')  -- Adjust to where delve is installed
+    },
+    {
+        type = "go",
+        name = "Attach",
+        request = "attach",
+        processId = require("dap.utils").pick_process,
+        -- program = "${workspaceFolder}",
+        dlvToolPath = vim.fn.exepath('dlv')
+    },
+    {
+        type = "go",
+        name = "Debug curr test",
+        request = "launch",
+        mode = "test",
+        program = "${file}",
+        dlvToolPath = vim.fn.exepath('dlv')
+    },
+    {
+        type = "go",
+        name = "Debug test",
+        request = "launch",
+        mode = "test",
+        program = "${workspaceFolder}",
+        dlvToolPath = vim.fn.exepath('dlv')
+    },
+
+}
+
+lvim.builtin.which_key.mappings["d"] = {
+    name = "+Debugger",
+    b = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "toggle breakpoint" },
+    c = { "<cmd>lua require'dap'.continue()<cr>", "continue" },
+    C = { "<cmd>lua require'dap'.run_to_cursor()<cr>", "continue to cursor" },
+    n = { "<cmd>lua require'dap'.step_over()<cr>", "next" },
+    s = { "<cmd>lua require'dap'.step_into()<cr>", "step" },
+    S = { "<cmd>lua require'dap'.step_out()<cr>", "step" },
+    e = { "<cmd>lua require'dap'.close()<cr>", "stop debugger" },
+    l = { "<cmd>lua require'dap'.list_breakpoints()<cr>", "list all breakpoint"},
+    r = { "<cmd>lua require'dap'.clear_breakpoints()<cr>","remove all breakpont"},
+    o = {"<cmd>lua require'dapui'.open()<cr>", "open debug ui window" },
+    x = {"<cmd>lua require'dapui'.close()<cr>", "close debug ui window" },
+    t = {"<cmd>lua require'dapui'.toggle()<cr>", "toggle debug ui window" },
+    f = {"<cmd>lua require'dapui'.float_element()<cr>", "get value" },
+    v = {"<cmd>lua require'dapui'.eval()<cr>", "get value" },
+}
+
+-- python debug
+dap.adapters.python = {
+  type = 'executable';
+  command = '/home/lijie/.local/share/nvim/dapinstall/python/bin/python';
+  args = { '-m', 'debugpy.adapter' };
+}
+
+dap.configurations.python = {
+  {
+    -- The first three options are required by nvim-dap
+    type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
+    request = 'launch';
+    name = "Launch file";
+    justMyCode = false;
+
+    -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+    program = "${file}"; -- This configuration will launch the current file if used.
+    pythonPath = function()
+      -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+      -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+      -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+      local cwd = vim.fn.getcwd()
+      if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+        return cwd .. '/venv/bin/python'
+      elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+        return cwd .. '/.venv/bin/python'
+      else
+        return '/usr/bin/python'
+      end
+    end;
+  },
+}
+
+require("dapui").setup()
+require("nvim-dap-virtual-text").setup()
+
+
+
+-- generic LSP settings
 lvim.lsp.on_attach_callback = function(client, _)
     -- close php auto format
     if client.name == "intelephense" then
@@ -360,27 +460,8 @@ lvim.plugins = {
         end,
   },
   {"theHamsta/nvim-dap-virtual-text"},
-  {'simrat39/symbols-outline.nvim'},
   {"rcarriga/nvim-dap-ui"},
-  {
-      "karb94/neoscroll.nvim",
-      -- event = "WinScrolled",
-      config = function()
-        require('neoscroll').setup({
-          -- All these keys will be mapped to their corresponding default scrolling animation
-          mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
-            '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
-          hide_cursor = true,          -- Hide cursor while scrolling
-          stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-          use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-          respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-          cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-          easing_function = nil,        -- Default easing function
-          pre_hook = nil,              -- Function to run before the scrolling animation starts
-          post_hook = nil,              -- Function to run after the scrolling animation ends
-        })
-      end
-  },
+  {'simrat39/symbols-outline.nvim'},
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
