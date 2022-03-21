f33dbbabc7e8c83599c4e58a0a89efc769892e2b
SDGLBL <lijieisgugu@gmail.com>
2022-03-21 23:05:08 +0800

    update

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 GO_INSTALLED=false
 CARGO_INSTALLED=false
 LUNARVIM_INSTALLED=false
+NEOVIM_INSTALLED=false
 
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 CONFIG_FOLDER=$SHELL_FOLDER/.config
@@ -51,67 +52,105 @@ function msg() {
   printf "%s\n" "$text"
 }
 
-# add git proxy for Mainland China users
-msg "Add git proxy for Mainland China users?"
-read -p "[y]es or [n]o (default: no) : " -r answer
-[ "$answer" != "${answer#[Yy]}" ] && printf 'mirror.ghproxy.com github.com\nmirror.ghproxy.com raw.githubusercontent.com\n' | sudo tee -a /etc/hosts
+function add_git_proxy() {
+  # add git proxy for Mainland China users
+  msg "Add git proxy for Mainland China users?"
+  read -p "[y]es or [n]o (default: no) : " -r answer
+  [ "$answer" != "${answer#[Yy]}" ] && printf 'mirror.ghproxy.com github.com\nmirror.ghproxy.com raw.githubusercontent.com\n' | sudo tee -a /etc/hosts
+}
 
 
 # install cargo
-if ! command_is_exists cargo; then
-  msg "Install cargo?"
-  read -p "[y]es or [n]o (default: no) : " -r answer
-  [ "$answer" != "${answer#[Yy]}" ] && curl https://sh.rustup.rs -sSf | sh
-  CARGO_INSTALLED=true
-  source $HOME/.cargo/env
-else
-  CARGO_INSTALLED=true
-fi
+function install_cargo() {
+  if ! command_is_exists cargo; then
+    msg "Install cargo?"
+    read -p "[y]es or [n]o (default: no) : " -r answer
+    [ "$answer" != "${answer#[Yy]}" ] && curl https://sh.rustup.rs -sSf | sh
+    CARGO_INSTALLED=true
+    source $HOME/.cargo/env
+  else
+    CARGO_INSTALLED=true
+  fi
+}
 
 # install go
 # https://github.com/canha/golang-tools-install-script
-if ! command_is_exists go; then
-  msg "Install go?"
-  read -p "[y]es or [n]o (default: no) : " -r answer
-  export GOPATH=$HOME/software/go
-  [ "$answer" != "${answer#[Yy]}" ] && wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash 
-  GO_INSTALLED=true
-  source $HOME/.bashrc
-else
-  GO_INSTALLED=true
-fi
+function install_go() {
+  if ! command_is_exists go; then
+    msg "Install go?"
+    read -p "[y]es or [n]o (default: no) : " -r answer
+    export GOPATH=$HOME/software/go
+    [ "$answer" != "${answer#[Yy]}" ] && wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash 
+    GO_INSTALLED=true
+    source $HOME/.bashrc
+  else
+    GO_INSTALLED=true
+  fi
+}
+
+# install neovim
+function install_neovim() {
+  if ! command_is_exists nvim; then
+    msg "Neovim is not installed yet, if you want install luarvim please install neovim first."
+    read -p "[y]es or [n]o (default: no) : " -r answer
+    if [ "$answer" != "${answer#[Yy]}" ]; then
+      if ! dir_is_exists $HOME/software; then
+        mkdir -p $HOME/software
+      fi
+      if [[ "$OSTYPE" =~ ^darwin ]]; then
+        wget --no-check-certificate https://github.com/neovim/neovim/releases/download/v0.6.1/nvim-macos.tar.gz -O $HOME/software/nvim.tar.gz
+        tar -xvf $HOME/software/nvim.tar.gz -C $HOME/software
+        mv $HOME/software/nvim-linux64 $HOME/software/nvim
+      elif [[ "$OSTYPE" =~ ^linux ]]; then
+        wget --no-check-certificate https://github.com/neovim/neovim/releases/download/v0.6.1/nvim-linux64.tar.gz -O $HOME/software/nvim.tar.gz
+        tar -xvf $HOME/software/nvim.tar.gz -C $HOME/software
+        mv $HOME/software/nvim-linux64 $HOME/software/nvim
+      fi
+      LUNARVIM_INSTALLED=true
+    fi
+  else
+    LUNARVIM_INSTALLED=true
+  fi
+}
 
 # install luarvim
-if ! command_is_exists lvim; then
-  msg "LunarVim is not installed. Would you like to install LunarVim dependencies?"
-  read -p "[y]es or [n]o (default: no) : " -r answer
-  [ "$answer" != "${answer#[Yy]}" ] && bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
-  LUNARVIM_INSTALLED=true
-else
-  LUNARVIM_INSTALLED=true
-fi
+function install_luarvim() {
+  if ! command_is_exists lvim; then
+    install_neovim
+    msg "LunarVim is not installed. Would you like to install LunarVim dependencies?"
+    read -p "[y]es or [n]o (default: no) : " -r answer
+    [ "$answer" != "${answer#[Yy]}" ] && bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+    LUNARVIM_INSTALLED=true
+  else
+    LUNARVIM_INSTALLED=true
+  fi
+}
 
 # install clipboard-provider
 # supoort osc52 copy remote vim clipboard
-if ! command_is_exists clipboard-provider; then
-  msg "Install clipboard-provider?"
-  read -p "[y]es or [n]o (default: no) : " -r answer
-  wget --no-check-certificate https://raw.githubusercontent.com/lotabout/dotfiles/master/bin/clipboard-provider && chmod +x clipboard-provider 
-  if ! dir_is_exists $HOME/.local/bin; then
-    mkdir -p $HOME/.local/bin
+function install_clipboard_provider() {
+  if ! command_is_exists clipboard-provider; then
+    msg "Install clipboard-provider?"
+    read -p "[y]es or [n]o (default: no) : " -r answer
+    wget --no-check-certificate https://raw.githubusercontent.com/lotabout/dotfiles/master/bin/clipboard-provider && chmod +x clipboard-provider 
+    if ! dir_is_exists $HOME/.local/bin; then
+      mkdir -p $HOME/.local/bin
+    fi
+    mv clipboard-provider $HOME/.local/bin/
   fi
-  mv clipboard-provider $HOME/.local/bin/
-fi
+}
 
 # install duf
-if ! command_is_exists duf; then
-  msg "Install duf?"
-  read -p "[y]es or [n]o (default: no) : " -r answer
-  [ "$answer" != "${answer#[Yy]}" ] && go install github.com/muesli/duf@master
-fi
+function install_duf() {
+  if ! command_is_exists duf; then
+    msg "Install duf?"
+    read -p "[y]es or [n]o (default: no) : " -r answer
+    [ "$answer" != "${answer#[Yy]}" ] && go install github.com/muesli/duf@master
+  fi
+}
 
 
-if $CARGO_INSTALLED; then
+function install_cargo_package() {
   # install bat
   if ! command_is_exists bat; then
     msg "Install bat?"
@@ -125,9 +164,9 @@ if $CARGO_INSTALLED; then
     read -p "[y]es or [n]o (default: no) : " -r answer
     [ "$answer" != "${answer#[Yy]}" ] && cargo install exa
   fi
-fi
+}
 
-if $GO_INSTALLED; then
+function innstall_go_package() {
   # add proxy for go
   msg "Use go proxy for Mainland China users?"
   read -p "[y]es or [n]o (default: no) : " -r answer
@@ -138,52 +177,76 @@ if $GO_INSTALLED; then
     read -p "[y]es or [n]o (default: no) : " -r answer
     [ "$answer" != "${answer#[Yy]}" ] && go install github.com/jesseduffield/lazygit@latest
   fi
-fi
+}
 
-if $LUNARVIM_INSTALLED; then
+function config_lunarvim() {
   if file_is_exists $LUNARVIM_CONFIG_FOLDER/config.lua; then
     mv $LUNARVIM_CONFIG_FOLDER/config.lua $LUNARVIM_CONFIG_FOLDER/config.lua.backup
   fi
   ln -s $CONFIG_FOLDER/lvim/config.lua ~/.config/lvim/config.lua
-fi
+
+}
 
 # if use tmux
-if command_is_exists tmux; then
-  if file_is_exists $HOME/.tmux.conf; then
-    mv $HOME/.tmux.conf $HOME/.tmux.conf.backup
+function config_tmux() {
+  if command_is_exists tmux; then
+    if file_is_exists $HOME/.tmux.conf; then
+      mv $HOME/.tmux.conf $HOME/.tmux.conf.backup
+    fi
+    ln -s $SHELL_FOLDER/.tmux.conf $HOME/.tmux.conf
+    # install tmux plugin
+    bash $HOME/.tmux/plugins/tpm/scripts/install_plugins.sh
   fi
-  ln -s $SHELL_FOLDER/.tmux.conf $HOME/.tmux.conf
-  # install tmux plugin
-  bash $HOME/.tmux/plugins/tpm/scripts/install_plugins.sh
-fi
+}
 
 # if current shell is bash 
-if [[ "$SHELL" =~ "bash" ]]; then
-  if file_is_exists $HOME/.bashrc; then
-    if file_is_exists $HOME/.config.sh; then
-      mv $HOME/.config.sh $HOME/.config.sh.backup
-    fi
-    ln -s $SHELL_FOLDER/.config.sh $HOME/.config.sh
-    temp="[[ ! -f ~/.config.sh ]] || source ~/.config.sh"
-    # if $temp not in $HOME/.bashrc and $HOME/.bashrc exist then append $temp to $HOME/.bashrc
-    if ! file_contain_string "$temp" $HOME/.bashrc; then
-      printf "$temp" >> $HOME/.bashrc
+function config_bash() {
+  if [[ "$SHELL" =~ "bash" ]]; then
+    if file_is_exists $HOME/.bashrc; then
+      if file_is_exists $HOME/.config.sh; then
+        mv $HOME/.config.sh $HOME/.config.sh.backup
+      fi
+      ln -s $SHELL_FOLDER/.config.sh $HOME/.config.sh
+      temp="[[ ! -f ~/.config.sh ]] || source ~/.config.sh"
+      # if $temp not in $HOME/.bashrc and $HOME/.bashrc exist then append $temp to $HOME/.bashrc
+      if ! file_contain_string "$temp" $HOME/.bashrc; then
+        printf "$temp" >> $HOME/.bashrc
+      fi
     fi
   fi
-fi
+}
 
 # if current shell is zsh
-if [[ "$SHELL" =~ "zsh" ]]; then
-  if file_is_exists $HOME/.zshrc; then
-    if file_is_exists $HOME/.config.sh; then
-      mv $HOME/.config.sh $HOME/.config.sh.backup
-    fi
-    ln -s $SHELL_FOLDER/.config.sh $HOME/.config.sh
+function config_zsh() {
+  if [[ "$SHELL" =~ "zsh" ]]; then
+    if file_is_exists $HOME/.zshrc; then
+      if file_is_exists $HOME/.config.sh; then
+        mv $HOME/.config.sh $HOME/.config.sh.backup
+      fi
+      ln -s $SHELL_FOLDER/.config.sh $HOME/.config.sh
 
-    temp="[[ ! -f ~/.config.sh ]] || source ~/.config.sh"
-    # if $temp not in $HOME/.zshrc and $HOME/.zshrc exist then append $temp to $HOME/.zshrc
-    if ! file_contain_string "$temp" $HOME/.zshrc; then
-      printf "$temp" >> $HOME/.zshrc
+      temp="[[ ! -f ~/.config.sh ]] || source ~/.config.sh"
+      # if $temp not in $HOME/.zshrc and $HOME/.zshrc exist then append $temp to $HOME/.zshrc
+      if ! file_contain_string "$temp" $HOME/.zshrc; then
+        printf "$temp" >> $HOME/.zshrc
+      fi
     fi
   fi
-fi
+}
+
+function main() {
+  add_git_proxy
+  install_cargo
+  install_go
+  install_luarvim
+  install_clipboard_provider
+  install_duf
+  install_cargo_package
+  innstall_go_package
+  config_lunarvim
+  config_tmux
+  config_bash
+  config_zsh
+}
+
+main
