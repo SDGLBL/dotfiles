65ba55249e9c9b298bc2bf7b0333ca94c7962e9b
SDGLBL <lijieisguge@gmail.com>
2022-05-06 18:21:50 +0800

    :wrench: Show LSP name

--- .config/nvim/lua/user/lualine/components.lua
+++ .config/nvim/lua/user/lualine/components.lua
@@ -101,14 +101,14 @@ return {
       end
 
       -- add formatter
-      local formatters = require "user.lsp.null-ls.formatters"
-      local supported_formatters = formatters.list_registered(buf_ft)
-      vim.list_extend(buf_client_names, supported_formatters)
+      -- local formatters = require "user.lsp.null-ls.formatters"
+      -- local supported_formatters = formatters.list_registered(buf_ft)
+      -- vim.list_extend(buf_client_names, supported_formatters)
 
       -- add linter
-      local linters = require "user.lsp.null-ls.linters"
-      local supported_linters = linters.list_registered(buf_ft)
-      vim.list_extend(buf_client_names, supported_linters)
+      -- local linters = require "user.lsp.null-ls.linters"
+      -- local supported_linters = linters.list_registered(buf_ft)
+      -- vim.list_extend(buf_client_names, supported_linters)
 
       return "[" .. table.concat(buf_client_names, ", ") .. "]"
     end,
