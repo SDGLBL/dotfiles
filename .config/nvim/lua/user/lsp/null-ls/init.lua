5ebbb7a242090757d4040a6686ab79d73dc4bead
SDGLBL <lijieisguge@gmail.com>
2022-05-08 18:42:32 +0800

    Fomat null-ls dir

--- .config/nvim/lua/user/lsp/null-ls/init.lua
+++ .config/nvim/lua/user/lsp/null-ls/init.lua
@@ -12,20 +12,30 @@ function M.setup()
   local diagnostics = null_ls.builtins.diagnostics
   -- local code_actions = null_ls.builtins.code_actions
 
-  null_ls.setup({
+  local opts = {
     debug = false,
     sources = {
       formatting.stylua,
       -- code_actions.refactoring,
       diagnostics.golangci_lint.with({
-        extra_args = { "run", "--fix=true", "--fast", "--out-format=json", "$DIRNAME", "--path-prefix", "$ROOT" },
+        extra_args = {
+          "run",
+          "--fix=true",
+          "--fast",
+          "--out-format=json",
+          "$DIRNAME",
+          "--path-prefix",
+          "$ROOT",
+        },
       }),
       -- diagnostics.flake8,
       -- formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
       -- formatting.black.with { extra_args = { "--fast" } },
       -- formatting.yapf,
     },
-  })
+  }
+
+  null_ls.setup(opts)
 end
 
 return M
