f5008e05b1199386aa67d052143f893f5bc1e2d8
SDGLBL <lijieisguge@gmail.com>
2022-05-14 11:40:57 +0800

    Update lsp configs

--- .config/nvim/lua/user/lsp/configs.lua
+++ .config/nvim/lua/user/lsp/configs.lua
@@ -1,28 +1,34 @@
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
-  return
+	return
 end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua", "tsserver" }
+local servers = {
+	"jsonls",
+	"sumneko_lua",
+	"tsserver",
+	"pylsp",
+	"gopls",
+	"ccls",
+	"rust_analyzer",
+}
 
 lsp_installer.setup({
-  ensure_installed = servers,
+	ensure_installed = servers,
 })
 
 for _, server in pairs(servers) do
-  local opts = {
-    on_attach = require("user.lsp.handlers").on_attach,
-    capabilities = require("user.lsp.handlers").capabilities,
-  }
+	local opts = {
+		on_attach = require("user.lsp.handlers").on_attach,
+		capabilities = require("user.lsp.handlers").capabilities,
+	}
 
-  local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
-  if has_custom_opts then
-    opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
-  end
+	local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
+	if has_custom_opts then
+		opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
+	end
 
-  lspconfig.jedi_language_server.setup({})
-  lspconfig.rust_analyzer.setup({})
-  lspconfig[server].setup(opts)
+	lspconfig[server].setup(opts)
 end
