34a3567f147f1125ddfb97a147d5c54f3ec58c64
SDGLBL <lijieisguge@gmail.com>
2022-05-30 00:00:07 +0800

    Add emmet_ls

--- .config/nvim/lua/user/lsp/configs.lua
+++ .config/nvim/lua/user/lsp/configs.lua
@@ -1,39 +1,40 @@
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
-  return
+	return
 end
 
 local lspconfig = require("lspconfig")
 
 local servers = {
-  "jsonls",
-  "sumneko_lua",
-  "tsserver",
-  "pylsp",
-  "gopls",
-  -- "ccls",
-  "clangd",
-  "cssls",
-  "yamlls",
-  "bashls",
-  "dockerls",
-  "rust_analyzer",
+	"jsonls",
+	"sumneko_lua",
+	"tsserver",
+	"pylsp",
+	"gopls",
+	-- "ccls",
+	"clangd",
+	"cssls",
+	"yamlls",
+	"bashls",
+	"dockerls",
+	"rust_analyzer",
+	"emmet_ls",
 }
 
 lsp_installer.setup({
-  ensure_installed = servers,
+	ensure_installed = servers,
 })
 
 for _, server in pairs(servers) do
-  local opts = {
-    on_attach = require("user.lsp.handlers").on_attach,
-    capabilities = require("user.lsp.handlers").capabilities,
-  }
+	local opts = {
+		on_attach = require("user.lsp.handlers").on_attach,
+		capabilities = require("user.lsp.handlers").capabilities,
+	}
 
-  local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
-  if has_custom_opts then
-    opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
-  end
+	local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
+	if has_custom_opts then
+		opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
+	end
 
-  lspconfig[server].setup(opts)
+	lspconfig[server].setup(opts)
 end
