7f68898a74ad3602ca3ef73e01b3bc0ee31649e6
SDGLBL <lijieisguge@gmail.com>
2022-06-19 18:34:11 +0800

    Add asm_lsp

--- .config/nvim/lua/user/lsp/configs.lua
+++ .config/nvim/lua/user/lsp/configs.lua
@@ -19,18 +19,19 @@ local servers = {
   "dockerls",
   "rust_analyzer",
   "emmet_ls",
+  "asm_lsp",
 }
 
 lsp_installer.setup {
   ensure_installed = servers,
 }
 
-for _, server in pairs(servers) do
-  local opts = {
-    on_attach = require("user.lsp.handlers").on_attach,
-    capabilities = require("user.lsp.handlers").capabilities,
-  }
+local opts = {
+  on_attach = require("user.lsp.handlers").on_attach,
+  capabilities = require("user.lsp.handlers").capabilities,
+}
 
+for _, server in pairs(servers) do
   local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
   if has_custom_opts then
     opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
