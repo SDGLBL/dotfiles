a6fa5f9344c04339ecd9743081509d9f44f211e4
SDGLBL <lijieisguge@gmail.com>
2022-11-06 22:56:11 +0800

    Add sniprun plugin

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -150,7 +150,7 @@ return packer.startup(function(use)
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
-  use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
+  use "tamago324/nlsp-settings.nvim" -- language server settings defined in json form
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
   use {
     "tami5/lspsaga.nvim",
@@ -328,6 +328,11 @@ return packer.startup(function(use)
     config = function()
       require("ccc").setup {}
     end,
+  }
+
+  use {
+    "michaelb/sniprun",
+    run = "bash ./install.sh",
   }
 
   -- Automatically set up your configuration after cloning packer.nvim
