8f6b2bb3b09f128ba5969da2c146bac9aecd53e8
SDGLBL <lijieisguge@gmail.com>
2022-11-07 10:23:03 +0800

    Change buffline default style as thick

--- .config/nvim/lua/user/bufferline.lua
+++ .config/nvim/lua/user/bufferline.lua
@@ -74,7 +74,7 @@ bufferline.setup {
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
     -- [focused and unfocused]. eg: { '|', '|' }
-    separator_style = "slant", -- "slant" | "thick" | "thin" | { 'any', 'any' }
+    separator_style = "thick", -- "slant" | "thick" | "thin" | { 'any', 'any' }
     enforce_regular_tabs = true,
     hover = {
       enabled = true,
