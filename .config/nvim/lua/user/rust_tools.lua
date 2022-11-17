eccf464806692c33ffce629c22a63e16cefe7324
SDGLBL <lijieisguge@gmail.com>
2022-11-17 10:43:32 +0800

    Update rust_tools keymaps

--- .config/nvim/lua/user/rust_tools.lua
+++ .config/nvim/lua/user/rust_tools.lua
@@ -7,9 +7,9 @@ rt.setup {
   server = {
     on_attach = function(_, bufnr)
       -- Hover actions
-      vim.keymap.set("n", "<Leader>la", rt.hover_actions.hover_actions, { buffer = bufnr })
+      vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
       -- Code action groups
-      vim.keymap.set("n", "<Leader>lA", rt.code_action_group.code_action_group, { buffer = bufnr })
+      vim.keymap.set("n", "<Leader>la", rt.code_action_group.code_action_group, { buffer = bufnr })
     end,
   },
 }
