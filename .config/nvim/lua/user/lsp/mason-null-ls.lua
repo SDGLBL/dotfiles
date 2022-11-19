dd2cb518e14e512b490dd31235bdb21dfc8c8f90
SDGLBL <lijieisguge@gmail.com>
2022-11-19 20:44:38 +0800

    Duplicated codespell

--- .config/nvim/lua/user/lsp/mason-null-ls.lua
+++ .config/nvim/lua/user/lsp/mason-null-ls.lua
@@ -69,6 +69,10 @@ require("mason-null-ls").setup_handlers {
     })
   end,
 
+  codespell = function(_, _)
+    null_ls.register(diagnostics.codespell)
+  end,
+
   write_good = function(_, _)
     null_ls.register(diagnostics.write_good)
   end,
