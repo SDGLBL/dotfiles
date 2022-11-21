fc63e47fb31a9bba83bd4dbff9942f899ea0f941
SDGLBL <lijieisguge@gmail.com>
2022-11-21 16:25:19 +0800

    Fix telescope diagnostics

--- .config/nvim/lua/user/lsp/init.lua
+++ .config/nvim/lua/user/lsp/init.lua
@@ -35,7 +35,7 @@ if ok_which_key then
       s = { "<cmd>Telescope lsp_document_symbols<cr>", "Doc Symbols" },
       S = { "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "Workspace Symbols" },
       q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Diagnostic List" },
-      w = { "<cmd>Telescope diagnostic<cr>", "Workspace Diagnostics" },
+      w = { "<cmd>Telescope diagnostics<cr>", "Workspace Diagnostics" },
       W = { '<cmd>lua require("telescope.builtin").diagnostics({ bufnr = 0 })<cr>', "Doc Diagnostics" },
       e = { "<cmd>Telescope quickfix<cr>", "Telescope Quickfix" },
     },
