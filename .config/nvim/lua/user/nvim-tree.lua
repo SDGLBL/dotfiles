631aaf53ccd5aff51fb497d9f3c439e979d82179
SDGLBL <lijieisguge@gmail.com>
2023-02-28 09:57:01 +0800

    Update nvim-tree config

--- .config/nvim/lua/user/nvim-tree.lua
+++ .config/nvim/lua/user/nvim-tree.lua
@@ -3,23 +3,6 @@ if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
-
--- Replaces auto_close
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
-vim.api.nvim_create_autocmd("BufEnter", {
-  nested = true,
-  callback = function()
-    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match "NvimTree_" ~= nil then
-      vim.cmd "quit"
-    end
-  end,
-})
-
 nvim_tree.setup {
   disable_netrw = true,
   hijack_netrw = true,
@@ -58,11 +41,6 @@ nvim_tree.setup {
     side = "left",
     mappings = {
       custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
     },
     number = false,
     relativenumber = false,
