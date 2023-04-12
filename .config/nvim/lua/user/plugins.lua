d0d3c15806f4ec826a4d50a53d761cb4c32d86ab
SDGLBL <lijieisguge@gmail.com>
2023-04-12 10:03:32 +0800

    Refactor transparent config

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -404,39 +404,16 @@ local plugins = {
     "xiyaowong/transparent.nvim",
     config = function()
       require("transparent").setup {
-        groups = { -- table: default groups
-          "Normal",
-          "NormalNC",
-          "Comment",
-          "Constant",
-          "Special",
-          "Identifier",
-          "Statement",
-          "PreProc",
-          "Type",
-          "Underlined",
-          "Todo",
-          "String",
-          "Function",
-          "Conditional",
-          "Repeat",
-          "Operator",
-          "Structure",
-          "LineNr",
-          "NonText",
-          "SignColumn",
-          "CursorLineNr",
-          "EndOfBuffer",
+        extra_groups = {
           "TelescopeBorder",
           "NvimTreeNormal",
           "NvimTreeNormalNC",
-          "MsgArea"
-        },
-        extra_groups = {}, -- table: additional groups that should be cleared
+          "MsgArea",
+        }, -- table: additional groups that should be cleared
         exclude_groups = {}, -- table: groups you don't want to clear
       }
     end,
-  }
+  },
 }
 
 return {
