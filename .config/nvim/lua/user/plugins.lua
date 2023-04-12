60822e8efaef937a323812e8fadab23a6dbc0fc2
SDGLBL <lijieisguge@gmail.com>
2023-04-12 22:52:16 +0800

    Add HopPreview to transparent

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -410,6 +410,7 @@ local plugins = {
           "NvimTreeNormal",
           "NvimTreeNormalNC",
           "MsgArea",
+          "HopPreview",
         }, -- table: additional groups that should be cleared
         exclude_groups = {}, -- table: groups you don't want to clear
       }
