0a37c268d0acd0d7ca60171bf01bba1674bf9b4f
SDGLBL <lijieisguge@gmail.com>
2023-04-15 14:54:17 +0800

    Add barbecue

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -196,4 +196,37 @@ return {
       }
     end,
   },
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    event = "BufWinEnter",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+    },
+    config = function()
+      -- triggers CursorHold event faster
+      vim.opt.updatetime = 200
+
+      require("barbecue").setup {
+        create_autocmd = false, -- prevent barbecue from updating itself automatically
+        kinds = require("utils.icons").kind,
+      }
+
+      vim.api.nvim_create_autocmd({
+        "WinScrolled", -- or WinResized on NVIM-v0.9 and higher
+        "BufWinEnter",
+        "CursorHold",
+        "InsertLeave",
+
+        -- include this if you have set `show_modified` to `true`
+        "BufModifiedSet",
+      }, {
+        group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+        callback = function()
+          require("barbecue.ui").update()
+        end,
+      })
+    end,
+  },
 }
