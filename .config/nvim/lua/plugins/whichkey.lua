8a7b0bec91e32cbf13bc7b40b642de9fd146b3f1
SDGLBL <lijieisguge@gmail.com>
2023-04-18 00:19:28 +0800

    Fix which del find files

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -19,6 +19,10 @@ return {
         ["q"] = { "<cmd>q!<CR>", "Quit" },
         ["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
         ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
+        ["f"] = {
+          "<cmd>Telescope find_files<cr>",
+          "Find files",
+        },
         b = {
           name = "Buffers",
           j = { "<cmd>BufferLinePick<cr>", "Jump" },
