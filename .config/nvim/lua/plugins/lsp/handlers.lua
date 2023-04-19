3de2a21df077e889ac9fe751ab8a1b56d36ca017
SDGLBL <lijieisguge@gmail.com>
2023-04-19 10:53:20 +0800

    show diagnostics source when more than one source

--- .config/nvim/lua/plugins/lsp/handlers.lua
+++ .config/nvim/lua/plugins/lsp/handlers.lua
@@ -16,7 +16,11 @@ M.setup = function()
 
   local config = {
     -- disable virtual text
-    virtual_text = true,
+    virtual_text = {
+      spacing = 4,
+      source = "if_many",
+      prefix = "●",
+    },
     -- show signs
     signs = {
       active = signs,
