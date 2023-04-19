2f14d606bfe0d3c78c5f50dc89053e38cc1a12bc
SDGLBL <lijieisguge@gmail.com>
2023-04-19 10:42:16 +0800

    Remove nlsp-settings.nvim

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -33,18 +33,6 @@ return {
           end)
         end,
       },
-      {
-        "tamago324/nlsp-settings.nvim",
-        config = function(_, _)
-          require("nlspsettings").setup {
-            config_home = vim.fn.stdpath "config" .. "/nlsp-settings",
-            local_settings_dir = ".nlsp-settings",
-            local_settings_root_markers_fallback = { ".git" },
-            append_default_schemas = true,
-            loader = "json",
-          }
-        end,
-      },
     },
     ---@class PluginLspOpts
     opts = {
