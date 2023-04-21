c54fb3b86d468d78e59966426b74d3cbce97e98b
SDGLBL <lijieisguge@gmail.com>
2023-04-21 08:51:33 +0800

    Fix python ts install

--- .config/nvim/lua/plugins/extras/lang/python.lua
+++ .config/nvim/lua/plugins/extras/lang/python.lua
@@ -4,7 +4,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "json", "json5", "jsonc" })
+        vim.list_extend(opts.ensure_installed, { "python" })
       end
     end,
   },
