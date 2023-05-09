7f4a12d6ab38721cf620e89654c1d048fd5a8602
SDGLBL <lijieisguge@gmail.com>
2023-05-09 21:36:58 +0800

    feat: 🎸 add splitjoin.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -148,4 +148,25 @@ return {
       })
     end,
   },
+
+  {
+    "bennypowers/splitjoin.nvim",
+    lazy = true,
+    keys = {
+      {
+        "gj",
+        function()
+          require("splitjoin").join()
+        end,
+        desc = "Join the object under cursor",
+      },
+      {
+        "g,",
+        function()
+          require("splitjoin").split()
+        end,
+        desc = "Split the object under cursor",
+      },
+    },
+  },
 }
