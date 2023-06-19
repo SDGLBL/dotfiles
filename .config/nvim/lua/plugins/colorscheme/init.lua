61b9eebd3bd34e405d58afb56812b59778ebe75c
SDGLBL <lijieisguge@gmail.com>
2023-06-19 23:47:26 +0800

    feat: 🎸 add midnight

--- .config/nvim/lua/plugins/colorscheme/init.lua
+++ .config/nvim/lua/plugins/colorscheme/init.lua
@@ -1,36 +1,39 @@
 return {
+  "folke/tokyonight.nvim",
+  "rebelot/kanagawa.nvim",
+  "tanvirtin/monokai.nvim",
+  "lunarvim/darkplus.nvim",
+  "tiagovla/tokyodark.nvim",
+  "ellisonleao/gruvbox.nvim",
+  "projekt0n/github-nvim-theme",
+  "marko-cerovac/material.nvim",
+  "bluz71/vim-nightfly-guicolors",
+  "felipeagc/fleet-theme-nvim",
   {
-    "folke/tokyonight.nvim",
-    "rebelot/kanagawa.nvim",
-    "tanvirtin/monokai.nvim",
-    "lunarvim/darkplus.nvim",
-    "tiagovla/tokyodark.nvim",
-    "ellisonleao/gruvbox.nvim",
-    "projekt0n/github-nvim-theme",
-    "marko-cerovac/material.nvim",
-    "bluz71/vim-nightfly-guicolors",
-    "felipeagc/fleet-theme-nvim",
-    {
-      "catppuccin/nvim",
-      name = "catppuccin",
-      config = function()
-        require("catppuccin").setup {
-          styles = {
-            comments = {},
-          },
-        }
-      end,
-    },
-    { "rose-pine/neovim", name = "rose-pine" },
-    {
-      "EdenEast/nightfox.nvim",
-      config = function()
-        require("nightfox").setup {}
-      end,
-    },
-    {
-      "olimorris/onedarkpro.nvim",
-      priority = 1000, -- Ensure it loads first
-    },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function()
+      require("catppuccin").setup {
+        styles = {
+          comments = {},
+        },
+      }
+    end,
+  },
+  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "EdenEast/nightfox.nvim",
+    config = function()
+      require("nightfox").setup {}
+    end,
+  },
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+  },
+  {
+    "dasupradyumna/midnight.nvim",
+    lazy = false,
+    priority = 1000,
   },
 }
