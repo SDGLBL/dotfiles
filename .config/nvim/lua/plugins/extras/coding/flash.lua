af3c7b1dbb535ac49300485a287f603390ecfa94
SDGLBL <lijieisguge@gmail.com>
2023-06-24 17:40:06 +0800

    refactor: 💡 update flash configuration

--- .config/nvim/lua/plugins/extras/coding/flash.lua
+++ .config/nvim/lua/plugins/extras/coding/flash.lua
@@ -15,12 +15,53 @@ return {
         desc = "Flash",
       },
       {
+        "sa",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump {
+            search = { forward = true, wrap = false, multi_window = false },
+          }
+        end,
+        desc = "Flash After",
+      },
+      {
+        "sb",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump {
+            search = { forward = false, wrap = false, multi_window = false },
+          }
+        end,
+        desc = "Flash After",
+      },
+      {
         "S",
         mode = { "n", "o", "x" },
         function()
           require("flash").treesitter()
         end,
         desc = "Flash Treesitter",
+      },
+      {
+        "<leader>t",
+        mode = { "n", "o", "x" },
+        function()
+          local win = vim.api.nvim_get_current_win()
+          local view = vim.fn.winsaveview()
+          require("flash").jump {
+            action = function(match, state)
+              state:hide()
+              vim.api.nvim_set_current_win(match.win)
+              vim.api.nvim_win_set_cursor(match.win, match.pos)
+              require("flash").treesitter()
+              vim.schedule(function()
+                vim.api.nvim_set_current_win(win)
+                vim.fn.winrestview(view)
+              end)
+            end,
+          }
+        end,
+        desc = "Flash Treesitter Copy",
       },
       {
         "r",
