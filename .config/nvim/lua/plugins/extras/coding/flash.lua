5b9e8347c8317e52b21349eb256cb4e8527d4174
SDGLBL <lijieisguge@gmail.com>
2023-07-16 00:08:56 +0800

    refactor: 💡 disable flash <leader>t keymap

--- .config/nvim/lua/plugins/extras/coding/flash.lua
+++ .config/nvim/lua/plugins/extras/coding/flash.lua
@@ -22,27 +22,27 @@ return {
         end,
         desc = "Flash Treesitter",
       },
-      {
-        "<leader>t",
-        mode = { "n", "o", "x" },
-        function()
-          local win = vim.api.nvim_get_current_win()
-          local view = vim.fn.winsaveview()
-          require("flash").jump {
-            action = function(match, state)
-              state:hide()
-              vim.api.nvim_set_current_win(match.win)
-              vim.api.nvim_win_set_cursor(match.win, match.pos)
-              require("flash").treesitter()
-              vim.schedule(function()
-                vim.api.nvim_set_current_win(win)
-                vim.fn.winrestview(view)
-              end)
-            end,
-          }
-        end,
-        desc = "Flash Treesitter Copy",
-      },
+      -- {
+      --   "<leader>t",
+      --   mode = { "n", "o", "x" },
+      --   function()
+      --     local win = vim.api.nvim_get_current_win()
+      --     local view = vim.fn.winsaveview()
+      --     require("flash").jump {
+      --       action = function(match, state)
+      --         state:hide()
+      --         vim.api.nvim_set_current_win(match.win)
+      --         vim.api.nvim_win_set_cursor(match.win, match.pos)
+      --         require("flash").treesitter()
+      --         vim.schedule(function()
+      --           vim.api.nvim_set_current_win(win)
+      --           vim.fn.winrestview(view)
+      --         end)
+      --       end,
+      --     }
+      --   end,
+      --   desc = "Flash Treesitter Copy",
+      -- },
       {
         "r",
         mode = "o",
