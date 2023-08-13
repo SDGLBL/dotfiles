3cf6a1a00eff0d084f870d9858d8470a1bf0da71
SDGLBL <lijieisguge@gmail.com>
2023-08-13 12:51:47 +0800

    feat: 🎸 update ideavimrc

--- .config/nvim/lua/plugins/extras/lang/go.lua
+++ .config/nvim/lua/plugins/extras/lang/go.lua
@@ -26,14 +26,6 @@ return {
           settings = {
             gopls = {
               usePlaceholders = false,
-              codelenses = {
-                gc_details = true,
-              },
-              analyses = {
-                fieldalignment = true,
-                nilness = true,
-                shadow = true,
-              },
               -- https://github.com/golang/tools/blob/master/gopls/doc/inlayHints.md
               hints = {
                 assignVariableTypes = true,
