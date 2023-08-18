ebf010a364a5b64340dde4c9123f30436e455485
SDGLBL <lijieisguge@gmail.com>
2023-08-19 01:31:13 +0800

    fix: 🐛 fix debug

--- .config/nvim/lua/plugins/extras/lang/rust.lua
+++ .config/nvim/lua/plugins/extras/lang/rust.lua
@@ -7,7 +7,7 @@ local function get_codelldb()
   local codelldb = mason_registry.get_package "codelldb"
   local extension_path = codelldb:get_install_path() .. "/extension/"
   local codelldb_path = extension_path .. "adapter/codelldb"
-  local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+  local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
   return codelldb_path, liblldb_path
 end
 
