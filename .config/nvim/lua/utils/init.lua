c5f45d71e81088277fec60dea0e37a08bc2aadda
SDGLBL <lijieisguge@gmail.com>
2023-08-19 10:44:41 +0800

    feat: 🎸 support linux and macos rust debug

--- .config/nvim/lua/utils/init.lua
+++ .config/nvim/lua/utils/init.lua
@@ -9,6 +9,21 @@ local function input_args()
   end
 end
 
+local function os_name()
+  local on = vim.loop.os_uname().sysname
+
+  if on == "Darwin" then
+    return "macOS"
+  elseif on == "Linux" then
+    return "Linux"
+  elseif on == "Windows" then
+    return "Windows"
+  else
+    return on
+  end
+end
+
 return {
   input_args = input_args,
+  os_name = os_name,
 }
