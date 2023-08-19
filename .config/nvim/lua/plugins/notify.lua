0dd67e6f8dc1bd2b5d2af01f2d1dd287aa5c34a7
SDGLBL <lijieisguge@gmail.com>
2023-08-20 01:13:22 +0800

    refactor: 💡 set nvim-notify top_down as false

--- .config/nvim/lua/plugins/notify.lua
+++ .config/nvim/lua/plugins/notify.lua
@@ -16,38 +16,36 @@ return {
       local icons = require "utils.icons"
 
       local defaults = {
-        active = true,
-        on_config_done = nil,
-        opts = {
-          ---@usage Animation style one of { "fade", "slide", "fade_in_slide_out", "static" }
-          stages = "fade",
+        ---@usage Animation style one of { "fade", "slide", "fade_in_slide_out", "static" }
+        stages = "static",
 
-          ---@usage Function called when a new window is opened, use for changing win settings/config
-          on_open = nil,
+        ---@usage Function called when a new window is opened, use for changing win settings/config
+        on_open = nil,
 
-          ---@usage Function called when a window is closed
-          on_close = nil,
+        ---@usage Function called when a window is closed
+        on_close = nil,
 
-          ---@usage timeout for notifications in ms, default 5000
-          timeout = 1500,
+        ---@usage timeout for notifications in ms, default 5000
+        timeout = 1500,
 
-          -- Render function for notifications. See notify-render()
-          render = "default",
+        -- Render function for notifications. See notify-render()
+        render = "default",
 
-          ---@usage highlight behind the window for stages that change opacity
-          background_colour = "Normal",
+        top_down = false,
 
-          ---@usage minimum width for notification windows
-          minimum_width = 50,
+        ---@usage highlight behind the window for stages that change opacity
+        background_colour = "Normal",
 
-          ---@usage Icons for the different levels
-          icons = {
-            ERROR = icons.diagnostics.Error,
-            WARN = icons.diagnostics.Warning,
-            INFO = icons.diagnostics.Hint,
-            DEBUG = icons.diagnostics.Debug,
-            TRACE = icons.diagnostics.Trace,
-          },
+        ---@usage minimum width for notification windows
+        minimum_width = 50,
+
+        ---@usage Icons for the different levels
+        icons = {
+          ERROR = icons.diagnostics.Error,
+          WARN = icons.diagnostics.Warning,
+          INFO = icons.diagnostics.Hint,
+          DEBUG = icons.diagnostics.Debug,
+          TRACE = icons.diagnostics.Trace,
         },
       }
 
