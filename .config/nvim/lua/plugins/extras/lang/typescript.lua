5cd1f986604a93fda4721c7e31daea258715dd7b
SDGLBL <lijieisguge@gmail.com>
2023-09-09 13:33:55 +0800

    add nvim-regexplainer

--- .config/nvim/lua/plugins/extras/lang/typescript.lua
+++ .config/nvim/lua/plugins/extras/lang/typescript.lua
@@ -232,6 +232,17 @@ return {
     },
   },
 
+  {
+    "bennypowers/nvim-regexplainer",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("regexplainer").setup()
+    end,
+  },
+
   -- test
   {
     "nvim-neotest/neotest",
