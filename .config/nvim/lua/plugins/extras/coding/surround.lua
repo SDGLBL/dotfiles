fcc0243aedd8b43bbf79f2dbabc6a345c38ead4d
SDGLBL <lijieisguge@gmail.com>
2023-12-09 12:01:34 +0800

    Enable nvim-surround plugin in coding extras configuration

--- .config/nvim/lua/plugins/extras/coding/surround.lua
+++ .config/nvim/lua/plugins/extras/coding/surround.lua
@@ -2,7 +2,7 @@ return {
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    enabled = false,
+    -- enabled = false,
     event = "VeryLazy",
     config = function()
       require("nvim-surround").setup {}
