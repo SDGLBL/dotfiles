04dedcf0a95c0876a8ff3c30859cd648fff04b00
SDGLBL <lijieisguge@gmail.com>
2023-12-12 01:05:16 +0800

    Add refresh and auto close functionality to the trouble plugin after running tests in the neotest plugin.

--- .config/nvim/lua/plugins/tdd.lua
+++ .config/nvim/lua/plugins/tdd.lua
@@ -114,6 +114,43 @@ return {
         },
       }, neotest_ns)
 
+      if require("utils").has "trouble.nvim" then
+        opts.consumers = opts.consumers or {}
+        -- Refresh and auto close trouble after running tests
+        ---@diagnostic disable-next-line: undefined-doc-name
+        ---@type neotest.Consumer
+        ---@diagnostic disable-next-line: assign-type-mismatch
+        opts.consumers.trouble = function(client)
+          client.listeners.results = function(adapter_id, results, partial)
+            if partial then
+              return
+            end
+
+            local tree = assert(client:get_position(nil, { adapter = adapter_id }))
+
+            local failed = 0
+            for pos_id, result in pairs(results) do
+              if result.status == "failed" and tree:get_key(pos_id) then
+                failed = failed + 1
+              end
+            end
+
+            vim.schedule(function()
+              local trouble = require "trouble"
+
+              if trouble.is_open() then
+                trouble.refresh()
+                if failed == 0 then
+                  trouble.close()
+                end
+              end
+            end)
+
+            return {}
+          end
+        end
+      end
+
       require("neotest").setup(opts)
     end,
   },
