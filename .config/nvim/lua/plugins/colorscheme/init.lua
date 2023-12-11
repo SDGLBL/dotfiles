8a6e7acb2ed192ae81ffeaab5ff72cd168a0eadd
SDGLBL <lijieisguge@gmail.com>
2023-12-12 00:30:09 +0800

    Refactored colorscheme plugin integration and added styles for various plugins.

--- .config/nvim/lua/plugins/colorscheme/init.lua
+++ .config/nvim/lua/plugins/colorscheme/init.lua
@@ -13,13 +13,46 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    config = function()
-      require("catppuccin").setup {
-        styles = {
-          comments = {},
+    opts = {
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
         },
-      }
-    end,
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+      styles = {
+        comments = {},
+      },
+    },
   },
   { "rose-pine/neovim", name = "rose-pine" },
   {
