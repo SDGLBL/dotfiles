425fe4403a0f984f69b497b16bd6cd6ca4eea187
SDGLBL <lijieisguge@gmail.com>
2023-12-14 00:17:43 +0800

    Added "gomod", "gowork", and "gosum" to the list of packages to ensure are installed for the Go language.

--- .config/nvim/lua/plugins/extras/lang/go.lua
+++ .config/nvim/lua/plugins/extras/lang/go.lua
@@ -8,7 +8,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "go" })
+        vim.list_extend(opts.ensure_installed, { "go", "gomod", "gowork", "gosum" })
       end
     end,
   },
