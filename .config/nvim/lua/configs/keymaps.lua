465401416476cd115f2ddf94191ede4c34a7557f
SDGLBL <lijieisguge@gmail.com>
2023-12-17 00:44:46 +0800

    Refactored keymaps.lua, removing the 'Hover' keymap and adding self quick delete /change keymaps.

--- .config/nvim/lua/configs/keymaps.lua
+++ .config/nvim/lua/configs/keymaps.lua
@@ -74,9 +74,6 @@ keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
--- Hover --
-keymap("n", "K", ":lua vim.lsp.buf.hover()<cr>", opts)
-
 -- self quick delete or change
 keymap("n", "dta", "dt&", opts)
 keymap("n", "dto", "dt|", opts)
