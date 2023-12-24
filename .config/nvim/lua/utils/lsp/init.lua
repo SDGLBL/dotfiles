320c6157857a22e2892e80d4406b8139aa886bed
SDGLBL <lijieisguge@gmail.com>
2023-12-24 21:30:24 +0800

    Add on_rename function for handling file renaming in LSP clients.

--- .config/nvim/lua/utils/lsp/init.lua
+++ .config/nvim/lua/utils/lsp/init.lua
@@ -225,4 +225,26 @@ function M.on_attach(on_attach, ...)
   vim.api.nvim_create_autocmd("LspAttach", opts)
 end
 
+---@param from string
+---@param to string
+function M.on_rename(from, to)
+  local clients = M.get_clients()
+  for _, client in ipairs(clients) do
+    if client.supports_method "workspace/willRenameFiles" then
+      ---@diagnostic disable-next-line: invisible
+      local resp = client.request_sync("workspace/willRenameFiles", {
+        files = {
+          {
+            oldUri = vim.uri_from_fname(from),
+            newUri = vim.uri_from_fname(to),
+          },
+        },
+      }, 1000, 0)
+      if resp and resp.result ~= nil then
+        vim.lsp.util.apply_workspace_edit(resp.result, client.offset_encoding)
+      end
+    end
+  end
+end
+
 return M
