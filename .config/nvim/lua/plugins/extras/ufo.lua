e2ce3da60a2bbb455e6ec9cd618442fd0964fe1b
SDGLBL <lijieisguge@gmail.com>
2024-04-01 15:03:02 +0800

    Removed "comment" from closed fold kinds in ufo NVim plugin configuration.

--- .config/nvim/lua/plugins/extras/ufo.lua
+++ .config/nvim/lua/plugins/extras/ufo.lua
@@ -136,7 +136,7 @@ return {
     opts = {
       open_fold_hl_timeout = 40,
       fold_virt_text_handler = handler,
-      close_fold_kinds_for_ft = { default = { "imports", "comment" } },
+      close_fold_kinds_for_ft = { default = { "imports" } },
       provider_selector = function(_, filetype, _)
         return ftMap[filetype]
       end,
