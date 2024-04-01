e2575366760a7bfb53e2e657e4d504bb9a090c65
SDGLBL <lijieisguge@gmail.com>
2024-04-01 15:03:21 +0800

    Added codecompanion plugin and disabled several AI-related plugins in Neovim configuration.

--- .config/nvim/lua/plugins/extras/ai.lua
+++ .config/nvim/lua/plugins/extras/ai.lua
@@ -1,27 +1,102 @@
 return {
   {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- Optional
+      {
+        "stevearc/dressing.nvim", -- Optional: Improves the default Neovim UI
+        opts = {},
+      },
+      {
+        "folke/edgy.nvim",
+        event = "VeryLazy",
+        init = function()
+          vim.opt.laststatus = 3
+          vim.opt.splitkeep = "screen"
+        end,
+        opts = {
+          right = {
+            { ft = "codecompanion", title = "Code Companion Chat", size = { width = 0.45 } },
+          },
+        },
+      },
+    },
+    cmds = { "CodeCompanion", "CodeCompanionChat", "CodeCompanionToggle", "CodeCompanionActions" },
+    keys = {
+      { "<leader>aa", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "Actions" },
+      { "<leader>at", "<cmd>CodeCompanionToggle<cr>", mode = { "n", "v" }, desc = "Toggle" },
+      -- { "<leader>sd", function() require("wtf").ai() end, desc = "Search Diagnostic with AI" },
+      -- { "<leader>sD", function() require("wtf").search() end, desc = "Search Diagnostic with Google" },
+    },
+    config = function(_, _)
+      require("codecompanion").setup {
+        adapters = {
+          chat = require("codecompanion.adapters").use("openai", {
+            env = {
+              api_key = "cmd:gpg --decrypt ~/.openai-api-key.gpg 2>/dev/null",
+            },
+            url = os.getenv "OPENAI_API_BASE" .. "/chat/completions",
+          }),
+          inline = require("codecompanion.adapters").use("openai", {
+            env = {
+              api_key = "cmd:gpg --decrypt ~/.openai-api-key.gpg 2>/dev/null",
+            },
+            url = os.getenv "OPENAI_API_BASE" .. "/chat/completions",
+          }),
+        },
+        actions = {
+          {
+            name = "Generate Git Message",
+            strategy = "inline",
+            description = "Generate a git commit message",
+            opts = { placement = "cursor" },
+            prompts = {
+              {
+                role = "system",
+                content = [[You are an expert software engineer.
+Review the provided context and diffs which are about to be committed to a git repo.
+Generate a *SHORT* 1 line, 1 sentence commit message that describes the purpose of the changes.
+The commit message MUST be in the past tense.
+It must describe the changes *which have been made* in the diffs!
+Reply with JUST the commit message, without quotes, comments, questions, etc!]],
+              },
+              {
+                role = "user",
+                content = [[ CONTEXT: ]] .. vim.fn.system "git diff --cached",
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
+
+  {
     "jackMort/ChatGPT.nvim",
+    enabled = false,
     cmd = { "ChatGPT", "ChatGPTRun", "ChatGPTActAs", "ChatGPTCompleteCode", "ChatGPTEditWithInstructions" },
     keys = {
       { "<leader>a", desc = "AI" },
-      { "<leader>ae", "<cmd>ChatGPTEditWithInstructions<cr>", mode = { "n", "v" }, desc = "Edit with Instructions" },
-      { "<leader>ac", "<cmd>ChatGPTRun complete_code<cr>", mode = { "n", "v" }, desc = "Complete Code" },
+      -- { "<leader>ae", "<cmd>ChatGPTEditWithInstructions<cr>", mode = { "n", "v" }, desc = "Edit with Instructions" },
+      -- { "<leader>ac", "<cmd>ChatGPTRun complete_code<cr>", mode = { "n", "v" }, desc = "Complete Code" },
       -- { "<leader>ag", "<cmd>ChatGPTRun grammar_correction<cr>", mode = { "n", "v" }, desc = "Grammar Correction" },
       { "<leader>at", "<cmd>ChatGPTRun translate chinese<cr>", mode = { "n", "v" }, desc = "Translate" },
       { "<leader>ad", "<cmd>ChatGPTRun docstring<cr>", mode = { "n", "v" }, desc = "Docstring" },
-      { "<leader>aa", "<cmd>ChatGPTRun add_tests<cr>", mode = { "n", "v" }, desc = "Add Tests" },
-      { "<leader>ao", "<cmd>ChatGPTRun optimize_code<cr>", mode = { "n", "v" }, desc = "Optimize Code" },
+      -- { "<leader>aa", "<cmd>ChatGPTRun add_tests<cr>", mode = { "n", "v" }, desc = "Add Tests" },
+      -- { "<leader>ao", "<cmd>ChatGPTRun optimize_code<cr>", mode = { "n", "v" }, desc = "Optimize Code" },
       -- { "<leader>as", "<cmd>ChatGPTRun summarize<cr>", mode = { "n", "v" }, desc = "Summarize Code" },
-      { "<leader>af", "<cmd>ChatGPTRun fix_bugs<cr>", mode = { "n", "v" }, desc = "Fix Bug" },
-      { "<leader>ax", "<cmd>ChatGPTRun explain_code<cr>", mode = { "n", "v" }, desc = "Explain Code" },
+      -- { "<leader>af", "<cmd>ChatGPTRun fix_bugs<cr>", mode = { "n", "v" }, desc = "Fix Bug" },
+      -- { "<leader>ax", "<cmd>ChatGPTRun explain_code<cr>", mode = { "n", "v" }, desc = "Explain Code" },
       -- { "<leader>acr", "<cmd>ChatGPTRun roxygen_edit<cr>", mode = { "n", "v" }, desc = "Roxygen Edit" },
-      { "<leader>aw", "<cmd>ChatGPTRun write_example<cr>", mode = { "n", "v" }, desc = "Write Example" },
-      {
-        "<leader>ar",
-        "<cmd>ChatGPTRun code_readability_analysis<cr>",
-        mode = { "n", "v" },
-        desc = "Readablility Analysis",
-      },
+      -- { "<leader>aw", "<cmd>ChatGPTRun write_example<cr>", mode = { "n", "v" }, desc = "Write Example" },
+      -- {
+      --   "<leader>ar",
+      --   "<cmd>ChatGPTRun code_readability_analysis<cr>",
+      --   mode = { "n", "v" },
+      --   desc = "Readablility Analysis",
+      -- },
     },
     opts = {
       openai_params = {
@@ -46,6 +121,7 @@ return {
 
   {
     "Bryley/neoai.nvim",
+    enabled = false,
     dependencies = {
       "MunifTanjim/nui.nvim",
     },
@@ -115,6 +191,7 @@ Reply with JUST the commit message, without quotes, comments, questions, etc! CO
 
   {
     "piersolenski/wtf.nvim",
+    enabled = false,
     dependencies = {
       "MunifTanjim/nui.nvim",
     },
