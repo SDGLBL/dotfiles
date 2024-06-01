d4e734197963354b6bbed3e00163bb565d798d76
SDGLBL <lijieisguge@gmail.com>
2024-06-01 11:07:30 +0800

    Added function to retrieve upvalue by name in utils module.

--- .config/nvim/lua/utils/init.lua
+++ .config/nvim/lua/utils/init.lua
@@ -183,4 +183,18 @@ function M.opts(name)
   return Plugin.values(plugin, "opts", false)
 end
 
+function M.get_upvalue(func, name)
+  local i = 1
+  while true do
+    local n, v = debug.getupvalue(func, i)
+    if not n then
+      break
+    end
+    if n == name then
+      return v
+    end
+    i = i + 1
+  end
+end
+
 return M
