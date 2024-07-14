40472eb6ab0292c176c6061f1c11f589162dc032
SDGLBL <lijieisguge@gmail.com>
2024-07-15 03:18:38 +0800

    Refactored window configuration in whichkey.lua

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -55,12 +55,12 @@ return {
           scroll_down = "<c-d>", -- binding to scroll down inside the popup
           scroll_up = "<c-u>", -- binding to scroll up inside the popup
         },
-        window = {
+        win = {
           border = "rounded", -- none, single, double, shadow
-          position = "bottom", -- bottom, top
-          margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+          -- position = "bottom", -- bottom, top
+          -- margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
           padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-          winblend = 0,
+          -- winblend = 0,
         },
         layout = {
           height = { min = 4, max = 25 }, -- min and max height of the columns
