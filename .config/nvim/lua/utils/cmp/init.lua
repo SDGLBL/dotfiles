372632e7d7d92dcdb9b02b0fd2e9635a24c4fa8f
SDGLBL <lijieisguge@gmail.com>
2024-08-14 00:48:35 +0800

    feat(nvim): improve cmp.confirm and add undo creation

--- .config/nvim/lua/utils/cmp/init.lua
+++ .config/nvim/lua/utils/cmp/init.lua
@@ -1,4 +1,5 @@
 local M = {}
+local utils = require "utils"
 
 M.snippet_replace = function(snippet, fn)
   return snippet:gsub("%$%b{}", function(m)
@@ -45,6 +46,28 @@ M.expand = function(snippet)
   -- Restore top-level session when needed
   if session then
     vim.snippet._session = session
+  end
+end
+
+-- This is a better implementation of `cmp.confirm`:
+--  * check if the completion menu is visible without waiting for running sources
+--  * create an undo point before confirming
+-- This function is both faster and more reliable.
+---@param opts? {select: boolean, behavior: cmp.ConfirmBehavior}
+function M.confirm(opts)
+  local cmp = require "cmp"
+  opts = vim.tbl_extend("force", {
+    select = true,
+    behavior = cmp.ConfirmBehavior.Insert,
+  }, opts or {})
+  return function(fallback)
+    if cmp.core.view:visible() or vim.fn.pumvisible() == 1 then
+      utils.create_undo()
+      if cmp.confirm(opts) then
+        return
+      end
+    end
+    return fallback()
   end
 end
 
--- .config/nvim/lua/utils/init.lua
+++ .config/nvim/lua/utils/init.lua
@@ -197,4 +197,11 @@ function M.get_upvalue(func, name)
   end
 end
 
+M.CREATE_UNDO = vim.api.nvim_replace_termcodes("<c-G>u", true, true, true)
+function M.create_undo()
+  if vim.api.nvim_get_mode().mode == "i" then
+    vim.api.nvim_feedkeys(M.CREATE_UNDO, "n", false)
+  end
+end
+
 return M
