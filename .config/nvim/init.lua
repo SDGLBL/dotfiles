1e1456bb5e3719e5b124737da07d189a41df02b9
SDGLBL <lijieisguge@gmail.com>
2024-08-15 21:56:14 +0800

    refactor: Enhance configuration and add WinLeave event handling in Neovim
    - Enabled format_on_save option and commented out specific language and feature configurations for clarity.
    - Added a WinLeave event to close TelescopePrompt if in insert mode, improving user experience.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,10 +8,13 @@ require("configs").setup {
   go = true,
   rust = true,
   python = true,
-  cpp = true,
-  typescript = true,
-  refactor = true,
-  ufo = true,
+  format_on_save = true,
+  -- cpp = true,
+  -- typescript = true,
+  -- refactor = true,
+  -- ufo = true,
+  -- tailwind = true,
+  -- hlargs = false,
   -- obsidian = true,
   -- nix = true,
   -- cpp = true,
@@ -42,6 +45,17 @@ require("configs").setup {
         callback = function()
           if vim.bo.ft == "codecompanion" then
             vim.wo.wrap = true
+          end
+        end,
+      },
+    },
+    {
+      "WinLeave",
+      {
+        group = "_close_telescope",
+        callback = function()
+          if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
           end
         end,
       },
