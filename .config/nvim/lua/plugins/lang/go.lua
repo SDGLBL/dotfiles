d5cbc053d0d035f0c26c1f795870a21da6b6f09a
SDGLBL <lijieisguge@gmail.com>
2024-08-15 21:54:40 +0800

    fix: disable usePlaceholders option in gopls configuration

--- .config/nvim/lua/plugins/lang/go.lua
+++ .config/nvim/lua/plugins/lang/go.lua
@@ -135,7 +135,7 @@ return {
             gopls = {
               gofumpt = true,
               semanticTokens = true,
-              usePlaceholders = true,
+              usePlaceholders = false,
               completeUnimported = true,
               staticcheck = true,
               directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
