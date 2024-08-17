68d6a3c72e02f7e1392a9821e0ce9b2efac9dba9
SDGLBL <lijieisguge@gmail.com>
2024-08-17 09:15:19 +0800

    fix: Correct indentation and formatting in .config.sh
    - Adjusted indentation for consistency in PATH and alias definitions.
    - Fixed alignment in conditional blocks and function definitions.
    - Ensured consistent spacing and formatting throughout the script.

--- .config.sh
+++ .config.sh
@@ -11,13 +11,13 @@ export PATH=$PATH:~/go/bin
 
 # add bin utils on macos
 if [[ "$OSTYPE" =~ ^darwin ]]; then
-	export PATH=/opt/homebrew/opt/binutils/bin:$PATH
-	export LDFLAGS="-L/opt/homebrew/opt/binutils/lib"
-	export CPPFLAGS="-I/opt/homebrew/opt/binutils/include"
+  export PATH=/opt/homebrew/opt/binutils/bin:$PATH
+  export LDFLAGS="-L/opt/homebrew/opt/binutils/lib"
+  export CPPFLAGS="-I/opt/homebrew/opt/binutils/include"
 
-	export LDFLAGS="-L/opt/homebrew/opt/curl/lib"
-	export CPPFLAGS="-I/opt/homebrew/opt/curl/include"
-	export PKG_CONFIG_PATH="/opt/homebrew/opt/curl/lib/pkgconfig"
+  export LDFLAGS="-L/opt/homebrew/opt/curl/lib"
+  export CPPFLAGS="-I/opt/homebrew/opt/curl/include"
+  export PKG_CONFIG_PATH="/opt/homebrew/opt/curl/lib/pkgconfig"
 fi
 
 export NEOVIDE_MULTIGRID=true
@@ -40,15 +40,15 @@ alias obsidianollama='nohup env OLLAMA_ORIGINS=app://obsidian.md\* ollama serve 
 
 # start vmod
 if [[ "$OSTYPE" =~ ^darwin ]]; then
-	alias vmod='source ~/.oh-my-zsh/custom/plugins/vi-mode/zsh-vi-mode.zsh'
+  alias vmod='source ~/.oh-my-zsh/custom/plugins/vi-mode/zsh-vi-mode.zsh'
 else
-	alias vmod='source ~/.zsh/vi-mode/vi-mode.zsh'
+  alias vmod='source ~/.zsh/vi-mode/vi-mode.zsh'
 fi
 
 # screen alias
 resume_screen() {
-	echo resume screen "$1"
-	screen -r "$1"
+  echo resume screen "$1"
+  screen -r "$1"
 }
 alias sr=resume_screen
 alias sls='screen -ls'
@@ -56,11 +56,11 @@ alias sw='screen -wipe'
 
 # Fuzzy find and change directory using fd with nested directory view
 fzf_cd() {
-	local dir
-	dir=$(fd --type d --hidden --follow --exclude .git . ${1:-.} 2>/dev/null |
-		fzf --preview 'tree -C {} | head -100' \
-			--bind 'ctrl-/:change-preview-window(down|hidden|)' \
-			--height 80% --reverse) && cd "$dir"
+  local dir
+  dir=$(fd --type d --hidden --follow --exclude .git . ${1:-.} 2>/dev/null |
+    fzf --preview 'tree -C {} | head -100' \
+      --bind 'ctrl-/:change-preview-window(down|hidden|)' \
+      --height 80% --reverse) && cd "$dir"
 }
 # Bind fzf_cd to a shorter command, e.g., 'cdf'
 alias cdf=fzf_cd
@@ -76,8 +76,8 @@ alias oldtop="/usr/bin/top"
 # alias man="tldr"
 # alias backup command
 function bak() {
-	# get file name from arg1
-	cp "$1" "$1"_"$(date +%y-%m-%d_%H:%M)"
+  # get file name from arg1
+  cp "$1" "$1"_"$(date +%y-%m-%d_%H:%M)"
 }
 
 alias simpleGitLog='git log --graph --pretty=oneline --abbrev-commit'
@@ -90,11 +90,11 @@ get_ftp_addr() { echo ftp://$(hostname)$(readlink -f "$1"); }
 
 # command_is_exists test command is exists
 command_is_exists() {
-	if command -v "$1" >/dev/null 2>&1; then
-		true
-	else
-		false
-	fi
+  if command -v "$1" >/dev/null 2>&1; then
+    true
+  else
+    false
+  fi
 }
 
 runcmd() { perl -e 'ioctl STDOUT, 0x5412, $_ for split //, <>'; }
@@ -102,57 +102,58 @@ writecmd() { perl -e 'ioctl STDOUT, 0x5412, $_ for split //, do{ chomp($_ = <>);
 
 # use exa
 if command_is_exists exa; then
-	alias l="exa -ll --all --icons"
-	alias lt="exa -ll -all --icons -T -L"
-	alias ll="exa -ll --icons"
-	alias llt="exa -ll --icons -T -L"
+  alias l="exa -ll --all --icons"
+  alias lt="exa -ll -all --icons -T -L"
+  alias ll="exa -ll --icons"
+  alias llt="exa -ll --icons -T -L"
 fi
 
 # support remote ssh copy (need terminal support osc52)
 # https://github.com/agriffis/skel/blob/master/neovim/bin/clipboard-provider
 if command_is_exists clipboard-provider; then
-	alias clp='clipboard-provider copy'
+  alias clp='clipboard-provider copy'
 fi
 
 # alias lazygit
 if command_is_exists lazygit; then
-	alias lg='lazygit -ucf ~/.config/lazygit/config.yml'
+  alias lg='lazygit -ucf ~/.config/lazygit/config.yml'
 fi
 
 # go proxy
 if command_is_exists go; then
-	export GOPROXY=https://goproxy.io,direct
+  export GOPROXY=https://goproxy.io,direct
 fi
 
 # GO_FLAGS
 function GO_FLAGS() {
-	flags=("-X" "main.goversion=$(go version)" "-X" "main.buildstamp=$(date -u '+%Y-%m-%d_%I:%M:%S%p')")
-	export flags
+  flags=("-X" "main.goversion=$(go version)" "-X" "main.buildstamp=$(date -u '+%Y-%m-%d_%I:%M:%S%p')")
+  export flags
 }
 
 if command_is_exists bat; then
-	alias cat="bat"
+  alias bat="bat --theme=gruvbox-dark"
+  alias cat="bat"
 fi
 
 # fzf config
 if command_is_exists fzf; then
-	export FZF_DEFAULT_OPTS='--preview-window=right:50% --layout=reverse --border'
+  export FZF_DEFAULT_OPTS='--preview-window=right:50% --layout=reverse --border'
 
-	# fh - repeat history
-	fh() {
-		([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -re 's/^\s*[0-9]+\s*//' | runcmd
-	}
-	# fhe - repeat history edit
-	fhe() {
-		([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -re 's/^\s*[0-9]+\s*//' | writecmd
-	}
+  # fh - repeat history
+  fh() {
+    ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -re 's/^\s*[0-9]+\s*//' | runcmd
+  }
+  # fhe - repeat history edit
+  fhe() {
+    ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -re 's/^\s*[0-9]+\s*//' | writecmd
+  }
 
-	# if had installed bat
-	if command_is_exists bat; then
-		alias pf='fzf --preview "bat --style=numbers --color=always --line-range :500 {}"'
-	else
-		alias pf="fzf --preview='less {}' --bind shift-up:preview-page-up,shift-down:preview-page-down"
-	fi
+  # if had installed bat
+  if command_is_exists bat; then
+    alias pf='fzf --preview "bat --style=numbers --color=always --line-range :500 {}"'
+  else
+    alias pf="fzf --preview='less {}' --bind shift-up:preview-page-up,shift-down:preview-page-down"
+  fi
 fi
 
 # vim setting
@@ -174,7 +175,7 @@ alias debug_prefix="GOMAXPROCS=1 GODEBUG=schedtrace=1000,scheddetail=1 mygo run"
 alias dlv="dlv --init ~/go/bin/dlv_config.init"
 
 if ! command_is_exists nix; then
-	[[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]] && . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+  [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]] && . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
 
 # fix rust blocking
