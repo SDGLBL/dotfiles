455b8534b41b92b57a81672340aa5c1a03c3e5a1
SDGLBL <lijieisguge@gmail.com>
2024-09-10 00:05:52 +0800

    feat(ui): remove unused bufferline groups for transparency plugin
    - Removed `BufferLineTabClose`, `BufferLineFill`, `BufferLineBackground`, and `BufferLineSeparator` from the `extra_groups` configuration of the `transparent` plugin.
    - Retained `BufferlineBufferSelected` and `BufferLineIndicatorSelected` to maintain necessary UI elements.

--- .config/nvim/lua/plugins/ui/init.lua
+++ .config/nvim/lua/plugins/ui/init.lua
@@ -95,11 +95,7 @@ return {
     config = function()
       require("transparent").setup {
         extra_groups = {
-          "BufferLineTabClose",
           "BufferlineBufferSelected",
-          "BufferLineFill",
-          "BufferLineBackground",
-          "BufferLineSeparator",
           "BufferLineIndicatorSelected",
 
           "SignColumn",
