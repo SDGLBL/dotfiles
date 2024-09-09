be1dfe2f201c38d9c9cf012def6eb8f2605c640c
SDGLBL <lijieisguge@gmail.com>
2024-09-10 00:23:55 +0800

    feat(nvim): disable LuaSnip plugin in coding completion setup
    - Added `enabled = false` to the LuaSnip plugin configuration in the coding completion setup.
    - This change ensures that LuaSnip is not loaded, potentially improving performance or reducing conflicts in the coding environment.

--- .config/nvim/lua/plugins/coding/cmp.lua
+++ .config/nvim/lua/plugins/coding/cmp.lua
@@ -228,6 +228,7 @@ return {
   {
     "L3MON4D3/LuaSnip",
     build = (not jit.os:find "Windows") and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp" or nil,
+    enabled = false,
     dependencies = {
       {
         "rafamadriz/friendly-snippets",
